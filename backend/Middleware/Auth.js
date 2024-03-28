b9024aadc9e6e0aac8e9c674af05435ad0e1133c
Nidhi <nidhis2k02@gmail.com>
2024-03-28 19:43:24 +0530

    RoutesAdded

--- backend/Middleware/Auth.js
+++ backend/Middleware/Auth.js
@@ -2,7 +2,7 @@ const jwt=require("jsonwebtoken")
 
 const auth= async(req,res,next)=>{
     const token = req.headers.authorization?.split(" ")[1]
-    console.log("Token:", token); 
+    //console.log("Token:", token); 
     if(token){
         jwt.verify(token,"masai",(err,decoded)=>{
             if(err){
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ const express = require("express")
 const { connection } = require("./config/db")
 const { userRouter } = require("./routes/user.route")
 const { performanceRoute } = require("./routes/performance.route")
+const { questionRoute } = require("./routes/question.route")
+const { quizRoute } = require("./routes/quiz.route")
 
 const app = express()
 app.use(express.json())
 app.use("/users",userRouter)
 app.use("/performance",performanceRoute)
+app.use("/question",questionRoute)
+app.use("/quiz",quizRoute)
 
 app.listen(process.env.port, async() => {
 	try {
