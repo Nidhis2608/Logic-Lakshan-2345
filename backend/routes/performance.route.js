3537897513613cdefb5d04b8a511da1a14427302
Nidhi <nidhis2k02@gmail.com>
2024-03-29 13:33:50 +0530

    Frontend

--- backend/routes/performance.route.js
+++ backend/routes/performance.route.js
@@ -15,16 +15,16 @@ performanceRoute.get("/",auth, async(req,res)=>{
     }
 })
 
-performanceRoute.post("/",auth,async(req,res)=>{
-  try{
-      const newPerformance= await PerformanceModel(req.body);
-        newPerformance.save()
-      res.status(201).json(newPerformance)
-    }
-    catch(err){
-        res.status(400).json({msg:err.message});
-    }
-})
+// performanceRoute.post("/",auth,async(req,res)=>{
+//   try{
+//       const newPerformance= await PerformanceModel(req.body);
+//         newPerformance.save()
+//       res.status(201).json(newPerformance)
+//     }
+//     catch(err){
+//         res.status(400).json({msg:err.message});
+//     }
+// })
 
 // performanceRoute.put("/",auth,async(req,res)=>{
 //     try{
@@ -45,44 +45,44 @@ performanceRoute.post("/",auth,async(req,res)=>{
 //     }
 // })
 
-performanceRoute.patch("/:performanceID",auth,async(req,res)=>{
-  const payload=req.body
-  const {performanceID}=req.params
-  try{
-      const performance=await PerformanceModel.findOne({_id:performanceID})
-      if(!performance){
-           return res.status(404).json({ msg: "Invalid id" });
-      } 
-      if(req.body.userID===performance.userID){
-    await PerformanceModel.findByIdAndUpdate({_id:performanceID},payload)
-    res.json({msg:`The performance with ID:${performanceID} has been updated`})
-      } else {
-          res.json({msg:"You don't have access to update someone else's tasks"})
-      }
-  }
-  catch (err){
-     res.json({err})
-  }
-})
+// performanceRoute.patch("/:performanceID",auth,async(req,res)=>{
+//   const payload=req.body
+//   const {performanceID}=req.params
+//   try{
+//       const performance=await PerformanceModel.findOne({_id:performanceID})
+//       if(!performance){
+//            return res.status(404).json({ msg: "Invalid id" });
+//       } 
+//       if(req.body.userID===performance.userID){
+//     await PerformanceModel.findByIdAndUpdate({_id:performanceID},payload)
+//     res.json({msg:`The performance with ID:${performanceID} has been updated`})
+//       } else {
+//           res.json({msg:"You don't have access to update someone else's tasks"})
+//       }
+//   }
+//   catch (err){
+//      res.json({err})
+//   }
+// })
 
-performanceRoute.delete("/:performanceID",auth,async(req,res)=>{
-  const {performanceID}=req.params
-  try{
-      const performance=await PerformanceModel.findOne({_id:performanceID})
-      if(!performance){
-           return res.status(404).json({ msg: "Invalid id" });
-      } 
-      if(req.body.userID===performance.userID){
-    await PerformanceModel.findByIdAndDelete({_id:performanceID})
-    res.json({msg:`The performance with ID:${performanceID} has been deleted`})
-      } else {
-          res.json({msg:"You don't have access to delete someone else's tasks"})
-      }
-  }
-  catch (err){
-     res.json({err})
-  }
-})
+// performanceRoute.delete("/:performanceID",auth,async(req,res)=>{
+//   const {performanceID}=req.params
+//   try{
+//       const performance=await PerformanceModel.findOne({_id:performanceID})
+//       if(!performance){
+//            return res.status(404).json({ msg: "Invalid id" });
+//       } 
+//       if(req.body.userID===performance.userID){
+//     await PerformanceModel.findByIdAndDelete({_id:performanceID})
+//     res.json({msg:`The performance with ID:${performanceID} has been deleted`})
+//       } else {
+//           res.json({msg:"You don't have access to delete someone else's tasks"})
+//       }
+//   }
+//   catch (err){
+//      res.json({err})
+//   }
+// })
 
 module.exports={
     performanceRoute
